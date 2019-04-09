b97e5bccf4c9e1866112402c0d89e725bccb9788
Lucy <kwang90@stuy.edu>
2019-04-08 23:10:04 -0400

    v0: code is not working

---
+++ Pair.java
@@ -0,0 +1,28 @@
+/**
+ Represent a pair of values of an arbitrary type,
+ for which the compiler helpfully imposes the requirement
+ that they be the SAME arbitrary type.
+
+ based on _Core Java_ v10, by Cay Horstmann
+ */
+public class Pair<T> 
+{
+    private T first;
+    private T second;
+
+    public Pair( T first, T second) { 
+        this.first =  first;
+        this.second = second; 
+        }
+
+    public String toString(){
+    	String string;
+    	return string = "( " + this.first + ", " + this.second + ")";
+    }
+
+    public T getFirst()  { return first; }
+    public T getSecond() { return second; }
+
+    public T setFirst( T value) { this.first = value;}
+    public T setSecond( T value) { this.second = value;}
+}
